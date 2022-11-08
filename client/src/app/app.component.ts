413924b77f871649bed9624d5b29e3d1212f8956
PeterKrizan <krizan.peter.fiit@gmail.com>
2022-11-08 20:54:13 +0100

    sidebar fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbMenuService, NbSidebarService } from '@nebular/theme';
+import { takeWhile } from 'rxjs';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { MENU_ITEMS, USER_MENU_ITEMS } from './nbutils/pages-menu';
+import { StateService } from './nbutils/state.service';
 import { AuthService } from './services/auth/auth.service';
+
 
 @Component({
   selector: 'app-root',
@@ -13,13 +17,25 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'client';
   users: any;
   menu: any;
-  private userSub : Subscription;
-  constructor(private http: HttpClient, private authService: AuthService) {
+  sidebar: any = {};
+  private alive = true;
+
+  private userSub: Subscription;
+  constructor(private http: HttpClient, private authService: AuthService, protected sidebarService: NbSidebarService, protected stateService: StateService, protected menuService: NbMenuService ) {
     this.menu = USER_MENU_ITEMS;
+    this.stateService.onSidebarState()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((sidebar: string) => {
+        this.sidebar = sidebar;
+      });
+
+      this.menuService.onItemSelect()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(() => this.sidebarService.compact('menu-sidebar'));
   }
 
   ngOnDestroy(): void {
-    if(this.userSub){
+    if (this.userSub) {
       this.userSub.unsubscribe();
     }
   }
@@ -27,13 +43,12 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.getUsers();
     this.authService.autologin();
-    this.userSub = this.authService.user.subscribe(user=>{
-      if(!!user)
-      {
+    this.userSub = this.authService.user.subscribe(user => {
+      if (!!user) {
         console.log("USER MENUCKO");
         this.menu = USER_MENU_ITEMS;
       }
-      else{
+      else {
         console.log("Strankove MENUCKO");
         this.menu = MENU_ITEMS;
       }
