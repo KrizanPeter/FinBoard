9fbf6291c2fd93d3c4267b739f7ca3543d5967a8
PeterKrizan <krizan.peter.fiit@gmail.com>
2022-11-15 21:41:38 +0100

    feat: updated minor bugs and mobile view

--- client/src/app/pages/create-guide/create-guide/create-guide.component.ts
+++ client/src/app/pages/create-guide/create-guide/create-guide.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { ResourceTemplateComponent } from '../../resources/resource-template/resource-template.component';
+import { SnapshotAgregateComponent } from '../../snapshot-agregate/snapshot-agregate/snapshot-agregate.component';
 
 @Component({
   selector: 'app-create-guide',
@@ -7,10 +9,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-guide.component.scss']
 })
 export class CreateGuideComponent implements OnInit {
-
+  @ViewChild(ResourceTemplateComponent) resourceTemplate:ResourceTemplateComponent;
+  @ViewChild(SnapshotAgregateComponent) aggregateTemplate:SnapshotAgregateComponent;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  refreshTemplates(){
+    this.resourceTemplate.ngOnInit();
+    this.aggregateTemplate.ngOnInit();
   }
 
   navigateToDashboardPage(){
