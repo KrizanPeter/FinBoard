e53d1480b8e5cf0752a90eaefb7425e788d17e88
PeterKrizan <krizan.peter.fiit@gmail.com>
2022-11-19 01:16:47 +0100

    feat: drop DB

--- API/Startup.cs
+++ API/Startup.cs
@@ -67,6 +67,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
+            var dropdb = Environment.GetEnvironmentVariable("DATABASE_DROP");
+            if (dropdb == "DATABASE_DROP")
+            {
+                context.Database.EnsureDeleted();
+            }
             context.Database.Migrate();
 
             if (env.IsDevelopment())
